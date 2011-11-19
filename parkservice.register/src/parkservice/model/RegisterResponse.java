f4b19364d0b81383623973cbb4627544933f491e
xiamike <xia.umd@gmail.com>
2011-11-19 11:23:32 -0500

    added servlet mapping xml and logic.  not tested

--- parkservice.register/src/parkservice/model/RegisterResponse.java
+++ parkservice.register/src/parkservice/model/RegisterResponse.java
@@ -4,10 +4,14 @@ public class RegisterResponse {
 	//the app can check for bad password, email, stuff like that.  
 	//but the server must check credit card information.  
 	private int responsecode;
+
 	/* 11: credit card valid
 	 * 22: credit card NOT valid
 	 * 33: could not connect to server*/
-
+	public RegisterResponse(int responsecode) {
+		super();
+		this.responsecode = responsecode;
+	}
 	/**
 	 * @return the responsecode
 	 */
--- parkservice.register/src/parkservice/resources/RegisterResource.java
+++ parkservice.register/src/parkservice/resources/RegisterResource.java
@@ -1,5 +1,43 @@
 package parkservice.resources;
 
-public class RegisterResource {
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.xml.bind.JAXBElement;
 
+import parkservice.model.RegisterRequest;
+import parkservice.model.RegisterResponse;
+
+import com.parq.server.dao.UserDao;
+import com.parq.server.dao.model.object.User;
+
+public class RegisterResource {
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public RegisterResponse login(JAXBElement<RegisterRequest> input){
+		RegisterRequest info = input.getValue();
+		User newUser = new User();
+		newUser.setEmail(info.getEmail());
+		newUser.setPassword(info.getPassword());
+		newUser.setUserName(info.getHolderName());
+		UserDao userDb = new UserDao();
+		//getByEmail throws exception when user is not in db, aka rs=null.
+		//thus user must be in db or this will throw exception.  
+		
+		/* user creation more verbose? */
+		boolean result = userDb.createNewUser(newUser);
+		/*  Dao layer needs to support these fields in table "User"
+ 			fname TEXT(64) NOT NULL, 
+ 			lname TEXT(64) NOT NULL,
+ 			parkstate TINYINT(1) NOT NULL,
+ 			parkloc POINT NOT NULL,
+		 */
+		if(result){
+			return new RegisterResponse(11);
+		}else{
+			return new RegisterResponse(33);
+		}
+	}
 }
