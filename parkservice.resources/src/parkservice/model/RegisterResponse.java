6ccc00f86f0cb2e05b16908ae4512b35f8bbe055
xiamike <xia.umd@gmail.com>
2011-11-26 17:06:27 -0500

    updated with working register

--- parkservice.resources/src/parkservice/model/RegisterResponse.java
+++ parkservice.resources/src/parkservice/model/RegisterResponse.java
@@ -1,32 +1,28 @@
 package parkservice.model;
-
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class RegisterResponse {
 	//the app can check for bad password, email, stuff like that.  
 	//but the server must check credit card information.  
-	private int responsecode;
+	String responsecode;
 
-	/* 11: credit card valid
-	 * 22: credit card NOT valid
-	 * 33: could not connect to server*/
-	public RegisterResponse(int responsecode) {
-		super();
-		this.responsecode = responsecode;
-	}
 	/**
 	 * @return the responsecode
 	 */
-	public int getResponsecode() {
+	public String getResponsecode() {
 		return responsecode;
 	}
 
 	/**
 	 * @param responsecode the responsecode to set
 	 */
-	public void setResponsecode(int responsecode) {
+	public void setResponsecode(String responsecode) {
 		this.responsecode = responsecode;
 	}
+
+	/* 11: credit card valid
+	 * 22: credit card NOT valid
+	 * 33: could not connect to server*/
 	
 }
