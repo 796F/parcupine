e5486b30ac1e082e270fa2f7500e7cb0db0cde2a
Michael Xia <xia.umd@gmail.com>
2012-01-19 12:35:59 -0500

    added scanner test, i cant test it

---
+++ PARQScanTest/PARQScanTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PARQScanTest
+//
+//  Created by Michael Xia on 1/19/12.
+//  Copyright (c) 2012 Iudex Projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PARQScanTest/PARQScanTest/ViewController.m
@@ -0,0 +1,91 @@
+//
+//  ViewController.m
+//  PARQScanTest
+//
+//  Created by Michael Xia on 1/19/12.
+//  Copyright (c) 2012 Iudex Projects. All rights reserved.
+//
+
+#import "ViewController.h"  
+
+@implementation ViewController
+
+- (IBAction)button {
+    //scan qr code, then display text inside the code.  
+    
+    //create new view for scanning
+    ZBarReaderViewController * reader = [ZBarReaderViewController new];
+    
+    //set the delegate to receive results
+    reader.readerDelegate = self;
+    
+    //configure reader for type of barcode
+    [reader.scanner setSymbology:ZBAR_QRCODE config:ZBAR_CFG_ENABLE to:0];
+    
+    //present the scanner
+    [self presentModalViewController:reader animated:YES];
+    
+}
+//this method is essentially onActivityResult()
+-(void) imagePickerController:(UIImagePickerController *)reader didFinishPickingMediaWithInfo:(NSDictionary *)info{
+    
+    id<NSFastEnumeration> results = [info objectForKey:ZBarReaderControllerResults];
+    ZBarSymbol* dunno;
+    for(dunno in results)
+        break;
+    
+    label.text = dunno.data;
+
+    //once we display the result string, dismiss the scanner.  
+    [reader dismissModalViewControllerAnimated:YES];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
