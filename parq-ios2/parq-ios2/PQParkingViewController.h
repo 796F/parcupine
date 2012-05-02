6de2651422a05b113f1dea5aed721721cdf180ad
Mark Yen <markyen@mit.edu>
2012-05-02 14:47:17 -0400

    Switched font to OCR B Std, implemented Start Timer UI changes

--- parq-ios2/parq-ios2/PQParkingViewController.h
+++ parq-ios2/parq-ios2/PQParkingViewController.h
@@ -10,6 +10,7 @@
 
 @interface PQParkingViewController : UITableViewController <UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UIButton *startButton;
+@property (weak, nonatomic) IBOutlet UIButton *unparkButton;
 @property (weak, nonatomic) IBOutlet UIImageView *paygFlag;
 @property (weak, nonatomic) IBOutlet UIImageView *prepaidFlag;
 @property (weak, nonatomic) IBOutlet UILabel *hours;
@@ -17,5 +18,9 @@
 @property (weak, nonatomic) IBOutlet UIImageView *paygCheck;
 @property (weak, nonatomic) IBOutlet UIImageView *prepaidCheck;
 @property (weak, nonatomic) IBOutlet UILabel *prepaidAmount;
+@property (weak, nonatomic) IBOutlet UIView *paygView;
+@property (weak, nonatomic) IBOutlet UIView *addressView;
+@property (weak, nonatomic) IBOutlet UIView *prepaidView;
+@property (weak, nonatomic) IBOutlet UIView *seeMapView;
 
 @end
--- parq-ios2/parq-ios2/PQParkingViewController.m
+++ parq-ios2/parq-ios2/PQParkingViewController.m
@@ -9,8 +9,13 @@
 #import "PQParkingViewController.h"
 #import "UIColor+Parq.h"
 
+@interface PQParkingViewController ()
+@property (nonatomic) BOOL timerStarted;
+@end
+
 @implementation PQParkingViewController
 @synthesize startButton;
+@synthesize unparkButton;
 @synthesize paygFlag;
 @synthesize prepaidFlag;
 @synthesize hours;
@@ -18,8 +23,74 @@
 @synthesize paygCheck;
 @synthesize prepaidCheck;
 @synthesize prepaidAmount;
+@synthesize paygView;
+@synthesize addressView;
+@synthesize prepaidView;
+@synthesize seeMapView;
+@synthesize timerStarted;
+
+- (IBAction)startTimer:(id)sender {
+    timerStarted = YES;
+    paygView.hidden = YES;
+    addressView.hidden = NO;
+    prepaidView.hidden = YES;
+    seeMapView.hidden = NO;
+    ((UIButton *)sender).hidden = YES;
+    unparkButton.hidden = NO;
+    [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]].userInteractionEnabled = NO;
+    [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:0]].accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    [self.tableView reloadData];
+}
+
+- (IBAction)unparkNow:(id)sender {
+    timerStarted = NO;
+    paygView.hidden = NO;
+    addressView.hidden = YES;
+    prepaidView.hidden = NO;
+    seeMapView.hidden = YES;
+    startButton.hidden = NO;
+    ((UIButton *)sender).hidden = YES;
+    [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]].userInteractionEnabled = YES;
+    [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:0]].accessoryType = UITableViewCellAccessoryNone;
+    [self.tableView reloadData];
+}
 
 #pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (!timerStarted) {
+        if (indexPath.section == 0) {
+            if (indexPath.row == 0) {
+                paygCheck.image = [UIImage imageNamed:@"check.png"];
+                prepaidCheck.image = [UIImage imageNamed:@"check_empty.png"];
+                paygFlag.hidden = NO;
+                prepaidFlag.hidden = YES;
+                prepaidAmount.text = @"Enter Amount";
+                prepaidAmount.textColor = [UIColor disabledTextColor];
+                hours.text = @"00";
+                minutes.text = @"00";
+            } else if (indexPath.row == 1) {
+                paygCheck.image = [UIImage imageNamed:@"check_empty.png"];
+                prepaidCheck.image = [UIImage imageNamed:@"check.png"];
+                paygFlag.hidden = YES;
+                prepaidFlag.hidden = NO;
+                prepaidAmount.text = @"1h 30m ($2.50)";
+                prepaidAmount.textColor = [UIColor activeTextColor];
+                hours.text = @"01";
+                minutes.text = @"30";
+            }
+        }
+    }
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    if (!timerStarted) {
+        return @"Select payment method";
+    } else {
+        return @"Your car\u2019s parking location";
+    }
+}
+
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
 	UIView *containerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 40)];
 	containerView.backgroundColor = [UIColor groupTableViewBackgroundColor];
@@ -33,41 +104,18 @@
 	label.text = [self tableView:tableView titleForHeaderInSection:section];
 	[containerView addSubview:label];
 
-    UIButton *abutton = [UIButton buttonWithType: UIButtonTypeInfoDark];
-    abutton.frame = CGRectMake(288, 12, 14, 14);
-//    [abutton addTarget: self action: @selector(addPage:)
-//      forControlEvents: UIControlEventTouchUpInside];
-    [containerView addSubview:abutton];
+    if (!timerStarted) {
+        UIButton *abutton = [UIButton buttonWithType: UIButtonTypeInfoDark];
+        abutton.frame = CGRectMake(288, 12, 14, 14);
+        //    [abutton addTarget: self action: @selector(addPage:)
+        //      forControlEvents: UIControlEventTouchUpInside];
+        [containerView addSubview:abutton];
+    }
 	return containerView;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
     return 36;
-}
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.section == 0) {
-        if (indexPath.row == 0) {
-            paygCheck.image = [UIImage imageNamed:@"check.png"];
-            prepaidCheck.image = [UIImage imageNamed:@"check_empty.png"];
-            paygFlag.hidden = NO;
-            prepaidFlag.hidden = YES;
-            prepaidAmount.text = @"Enter Amount";
-            prepaidAmount.textColor = [UIColor disabledTextColor];
-            hours.text = @"00";
-            minutes.text = @"00";
-        } else if (indexPath.row == 1) {
-            paygCheck.image = [UIImage imageNamed:@"check_empty.png"];
-            prepaidCheck.image = [UIImage imageNamed:@"check.png"];
-            paygFlag.hidden = YES;
-            prepaidFlag.hidden = NO;
-            prepaidAmount.text = @"1h 30m ($2.50)";
-            prepaidAmount.textColor = [UIColor activeTextColor];
-            hours.text = @"01";
-            minutes.text = @"30";
-        }
-    }
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 #pragma mark - Memory
@@ -83,11 +131,13 @@
 - (void)viewDidLoad{
     [super viewDidLoad];
 
-    [startButton setBackgroundImage:[[UIImage imageNamed:@"green_button.png"] stretchableImageWithLeftCapWidth:14.0 topCapHeight:0.0] forState:UIControlStateNormal];
-//    [startButton addTarget:self action:@selector(buttonPressed) forControlEvents:UIControlEventTouchUpInside];
+    timerStarted = NO;
 
-    hours.font = [UIFont fontWithName:@"OCR A Std" size:60];
-    minutes.font = [UIFont fontWithName:@"OCR A Std" size:60];
+    [startButton setBackgroundImage:[[UIImage imageNamed:@"green_button.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 14, 0, 14)] forState:UIControlStateNormal];
+    [unparkButton setBackgroundImage:[[UIImage imageNamed:@"red_button.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 14, 0, 14)] forState:UIControlStateNormal];
+
+    hours.font = [UIFont fontWithName:@"OCR B Std" size:60];
+    minutes.font = [UIFont fontWithName:@"OCR B Std" size:60];
 }
 
 - (void)viewDidUnload
@@ -100,6 +150,11 @@
     [self setPaygCheck:nil];
     [self setPrepaidCheck:nil];
     [self setPrepaidAmount:nil];
+    [self setPaygView:nil];
+    [self setAddressView:nil];
+    [self setPrepaidView:nil];
+    [self setSeeMapView:nil];
+    [self setUnparkButton:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
