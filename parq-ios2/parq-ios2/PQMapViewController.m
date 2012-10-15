796F/parcupine

commit dfe54841118fe4f749ae2a7a209759845e8766f0

In None mode - allow pressing Just Park Me - but switch back to Availability

parq-ios2/parq-ios2/PQMapViewController.m
@@ -428,6 +428,14 @@ typedef struct{
 // @PILOT fixed location - future will zoom to closest spot to center
 -(IBAction)justParkMeButtonPressed:(id)sender{
     DLog(@"");
+    // Allow use to "Just Park Me" in availability bar "None" mode by switching to Availability
+    if (self.availabilitySelectionBar.selectedSegmentIndex == 2) {
+        self.availabilitySelectionBar.selectedSegmentIndex = 0;
+        gradientIcon.image = [UIImage imageNamed:@"gradient_avail"];
+        displayedData = kAvailabilityData;
+        [self mapView:map regionDidChangeAnimated:NO];
+    }
+
     [dataLayer logString:[NSString stringWithFormat:@"%s", __PRETTY_FUNCTION__]];
     CLLocationCoordinate2D coord = CLLocationCoordinate2DMake(42.357835,-71.094333);
     [map setRegion:MKCoordinateRegionMakeWithDistance(coord, SPOT_LEVEL_REGION_METERS, SPOT_LEVEL_REGION_METERS) animated:YES];
@@ -1833,19 +1841,19 @@ typedef struct{
 #pragma mark - MAP TOOLBARS
 - (IBAction)availabilityBarTapped {
     switch (self.availabilitySelectionBar.selectedSegmentIndex) {
-        case 0:
+        case 0:     // Availability
             [dataLayer logString:[NSString stringWithFormat:@"%s %@", __PRETTY_FUNCTION__, @"avail"]];
             gradientIcon.image = [UIImage imageNamed:@"gradient_avail"];
             displayedData = kAvailabilityData;
             [self mapView:map regionDidChangeAnimated:NO];
             break;
-        case 1:
+        case 1:     // Price
             [dataLayer logString:[NSString stringWithFormat:@"%s %@", __PRETTY_FUNCTION__, @"price"]];
             gradientIcon.image = [UIImage imageNamed:@"gradient_price"];
             displayedData = kPriceData;
             [self mapView:map regionDidChangeAnimated:NO];
             break;
-        case 2:
+        case 2:     // None
             [dataLayer logString:[NSString stringWithFormat:@"%s %@", __PRETTY_FUNCTION__, @"none"]];
             gradientIcon.image = [UIImage imageNamed:@"gradient_none"];
             displayedData = kNoneData;
@@ -1989,6 +1997,7 @@ typedef struct{
 
     }];
 }
+
 -(void) showMoreTextBox{
     [UIView animateWithDuration:.35 animations:^{
         //show

parq-ios2/parq-ios2/en.lproj/MainStoryboard.storyboard
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<document type="com.apple.InterfaceBuilder3.CocoaTouch.Storyboard.XIB" version="2.0" toolsVersion="2840" systemVersion="12B19" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" initialViewController="2">
+<document type="com.apple.InterfaceBuilder3.CocoaTouch.Storyboard.XIB" version="2.0" toolsVersion="2843" systemVersion="12B19" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" initialViewController="2">
     <dependencies>
-        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="1926"/>
+        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="1929"/>
     </dependencies>
     <scenes>
         <!--Map View Controller-->

commit 3d37f0972c6713d694fbc2cc3165e62ae9dfeff2

Fix bug caused by  dfe5484111 - rename noneButtonPressed to mockData because that's what the button actually does

parq-ios2/parq-ios2/PQMapViewController.m
@@ -2008,6 +2008,47 @@ typedef struct{
     }];
 }
 
+- (IBAction)loadMockData:(id)sender {
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.labelText = @"Loading Data...";
+    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+        
+        [dataLayer loadMockData];
+        [networkLayer loadSpotData];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [MBProgressHUD hideHUDForView:self.view animated:YES];
+        });
+    });
+    ((UIButton*)sender).hidden = YES;
+    
+    
+    //[self hideMoreTextBox];
+    //    [dataLayer logString:[NSString stringWithFormat:@"%s", __PRETTY_FUNCTION__]];
+    //    int olduitype = [dataLayer UIType];
+    //    if(olduitype==3) olduitype = -1;
+    //    [dataLayer setUIType:olduitype+1];
+    //    NSString* string = [NSString stringWithFormat:@"uitype = %d", olduitype+1];
+    //    UIAlertView* alert = [[UIAlertView alloc] initWithTitle:nil message:string delegate:self cancelButtonTitle:@"ok" otherButtonTitles:nil];
+    //    [alert show];
+    
+    
+    //    self.map = [[MKMapView alloc] initWithFrame:CGRectMake(0, 44, 320, 416)];
+    
+    //    [self clearMap];
+    //    [networkLayer testAsync];
+    
+    //    int loop = 0;
+    //    NSLog(@"map annotation count%d\n", map.annotations.count);
+    //    for(PQSpotAnnotation* anno in map.annotations){
+    //        loop++;
+    //        NSLog(@"looped %d\n", loop);
+    //        MKAnnotationView* view = [map viewForAnnotation:anno];
+    //        view.image = [UIImage imageNamed:@"spot_occupied.png"];
+    //    }
+}
+
+
 - (void)keyboardWillShow:(NSNotification *)note { 
 
     if(topSearchBar.selectedScopeButtonIndex==1){

parq-ios2/parq-ios2/en.lproj/MainStoryboard.storyboard
@@ -73,7 +73,7 @@
                                     <color key="titleColor" white="1" alpha="1" colorSpace="calibratedWhite"/>
                                 </state>
                                 <connections>
-                                    <action selector="noneButtonPressed:" destination="2" eventType="touchUpInside" id="9Mt-CY-Iaa"/>
+                                    <action selector="loadMockData:" destination="2" eventType="touchUpInside" id="orN-Qm-P0g"/>
                                 </connections>
                             </button>
                             <view opaque="NO" contentMode="scaleToFill" id="xax-VC-fGh">
@@ -1680,6 +1680,166 @@ AQABAAEAAQAB//+dkAEA//+PgAAE//+dkAEI//+dkAEMUERUAFBTVABQV1QAUFBUAAAAAAEAAAABA
         <image name="right_arrow.png" width="20" height="20"/>
         <image name="settings.png" width="20" height="21"/>
     </resources>
+    <classes>
+        <class className="BookmarkCell" superclassName="UITableViewCell">
+            <source key="sourceIdentifier" type="project" relativePath="./Classes/BookmarkCell.h"/>
+            <relationships>
+                <relationship kind="outlet" name="bookmarkName" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="editBookmarkNameField" candidateClass="UITextField"/>
+                <relationship kind="outlet" name="editMe" candidateClass="UIButton"/>
+                <relationship kind="outlet" name="goMap" candidateClass="UIButton"/>
+                <relationship kind="outlet" name="minus" candidateClass="UIButton"/>
+            </relationships>
+        </class>
+        <class className="EditSettingsViewController" superclassName="UIViewController">
+            <source key="sourceIdentifier" type="project" relativePath="./Classes/EditSettingsViewController.h"/>
+            <relationships>
+                <relationship kind="outlet" name="confirmNewValue" candidateClass="UITextField"/>
+                <relationship kind="outlet" name="currentValue" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="editNewValue" candidateClass="UITextField"/>
+                <relationship kind="outlet" name="saveButton" candidateClass="UIBarButtonItem"/>
+            </relationships>
+        </class>
+        <class className="LoginNavigationController" superclassName="UINavigationController">
+            <source key="sourceIdentifier" type="project" relativePath="./Classes/LoginNavigationController.h"/>
+        </class>
+        <class className="PQBookmarksViewController" superclassName="UIViewController">
+            <source key="sourceIdentifier" type="project" relativePath="./Classes/PQBookmarksViewController.h"/>
+            <relationships>
+                <relationship kind="outlet" name="bookmarkSelectionBar" candidateClass="UISegmentedControl"/>
+                <relationship kind="outlet" name="leftButton" candidateClass="UIBarButtonItem"/>
+                <relationship kind="outlet" name="midButton" candidateClass="UIBarButtonItem"/>
+                <relationship kind="outlet" name="rightButton" candidateClass="UIBarButtonItem"/>
+                <relationship kind="outlet" name="table" candidateClass="UITableView"/>
+            </relationships>
+        </class>
+        <class className="PQEditBookmarkViewController" superclassName="UIViewController">
+            <source key="sourceIdentifier" type="project" relativePath="./Classes/PQEditBookmarkViewController.h"/>
+            <relationships>
+                <relationship kind="outlet" name="cancelButton" candidateClass="UIBarButtonItem"/>
+                <relationship kind="outlet" name="map" candidateClass="MKMapView"/>
+                <relationship kind="outlet" name="saveButton" candidateClass="UIBarButtonItem"/>
+            </relationships>
+        </class>
+        <class className="PQLoginViewController" superclassName="UITableViewController">
+            <source key="sourceIdentifier" type="project" relativePath="./Classes/PQLoginViewController.h"/>
+            <relationships>
+                <relationship kind="outlet" name="confirmPasswordField" candidateClass="UITextField"/>
+                <relationship kind="outlet" name="emailField" candidateClass="UITextField"/>
+                <relationship kind="outlet" name="entireScreen" candidateClass="UIView"/>
+                <relationship kind="outlet" name="licensePlateField" candidateClass="UITextField"/>
+                <relationship kind="outlet" name="navBar" candidateClass="UINavigationBar"/>
+                <relationship kind="outlet" name="passwordField" candidateClass="UITextField"/>
+                <relationship kind="outlet" name="registerButton" candidateClass="UIButton"/>
+                <relationship kind="outlet" name="submitButton" candidateClass="UIButton"/>
+                <relationship kind="outlet" name="whiteButton" candidateClass="UIButton"/>
+            </relationships>
+        </class>
+        <class className="PQMapViewController" superclassName="UIViewController">
+            <source key="sourceIdentifier" type="project" relativePath="./Classes/PQMapViewController.h"/>
+            <relationships>
+                <relationship kind="outlet" name="availabilitySelectionBar" candidateClass="UISegmentedControl"/>
+                <relationship kind="outlet" name="availabilitySelectionView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="bottomSpotSelectionBar" candidateClass="UISegmentedControl"/>
+                <relationship kind="outlet" name="bottomSpotSelectionView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="cancelDropPinButton" candidateClass="UIButton"/>
+                <relationship kind="outlet" name="dropPinButton" candidateClass="UIButton"/>
+                <relationship kind="outlet" name="dropPinSelectionView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="findMeButton" candidateClass="UIButton"/>
+                <relationship kind="outlet" name="gradientIcon" candidateClass="UIImageView"/>
+                <relationship kind="outlet" name="justParkSelectionView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="map" candidateClass="MKMapView"/>
+                <relationship kind="outlet" name="mockDataButton" candidateClass="UIButton"/>
+                <relationship kind="outlet" name="navigationBar" candidateClass="UINavigationItem"/>
+                <relationship kind="outlet" name="parkMeButton" candidateClass="UIButton"/>
+                <relationship kind="outlet" name="popOutSpotNumberField" candidateClass="UITextField"/>
+                <relationship kind="outlet" name="popOutSpotNumberView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="topSearchBar" candidateClass="UISearchBar"/>
+                <relationship kind="outlet" name="topSpotSelectionBar" candidateClass="UISegmentedControl"/>
+                <relationship kind="outlet" name="topSpotSelectionView" candidateClass="UIView"/>
+            </relationships>
+        </class>
+        <class className="PQParkedCarMapViewController" superclassName="UIViewController">
+            <source key="sourceIdentifier" type="project" relativePath="./Classes/PQParkedCarMapViewController.h"/>
+            <relationships>
+                <relationship kind="outlet" name="map" candidateClass="MKMapView"/>
+            </relationships>
+        </class>
+        <class className="PQParkingViewController" superclassName="UITableViewController">
+            <source key="sourceIdentifier" type="project" relativePath="./Classes/PQParkingViewController.h"/>
+            <relationships>
+                <relationship kind="outlet" name="addressCellView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="addressLabel" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="cancelButton" candidateClass="UIBarButtonItem"/>
+                <relationship kind="outlet" name="colon" candidateClass="UIImageView"/>
+                <relationship kind="outlet" name="datePicker" candidateClass="UIDatePicker"/>
+                <relationship kind="outlet" name="doneButton" candidateClass="UIBarButtonItem"/>
+                <relationship kind="outlet" name="expiresAtTime" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="extendAmount" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="extendButton" candidateClass="UIButton"/>
+                <relationship kind="outlet" name="hours" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="limitUnit" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="limitValue" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="meter" candidateClass="UIImageView"/>
+                <relationship kind="outlet" name="minutes" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="paygCellView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="paygCheck" candidateClass="UIImageView"/>
+                <relationship kind="outlet" name="paygFlag" candidateClass="UIImageView"/>
+                <relationship kind="outlet" name="prepaidAmount" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="prepaidCellView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="prepaidCheck" candidateClass="UIImageView"/>
+                <relationship kind="outlet" name="prepaidFlag" candidateClass="UIImageView"/>
+                <relationship kind="outlet" name="rateDenominator" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="rateNumerator" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="remainingAmount" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="seeMapCellView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="startButton" candidateClass="UIButton"/>
+                <relationship kind="outlet" name="timeRemainingCellView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="timeToAddCellView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="unparkButton" candidateClass="UIButton"/>
+            </relationships>
+        </class>
+        <class className="PQPaymentMethodsViewController" superclassName="UITableViewController">
+            <source key="sourceIdentifier" type="project" relativePath="./Classes/PQPaymentMethodsViewController.h"/>
+        </class>
+        <class className="PQSettingsViewController" superclassName="UITableViewController">
+            <source key="sourceIdentifier" type="project" relativePath="./Classes/PQSettingsViewController.h"/>
+            <relationships>
+                <relationship kind="outlet" name="addrCellView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="addrLabel" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="balanceCellView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="balanceLabel" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="emailCellView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="emailLabel" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="nameCellView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="nameLabel" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="plateCellView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="plateLabel" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="soundCellView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="ssnCellView" candidateClass="UIView"/>
+                <relationship kind="outlet" name="ssnLabel" candidateClass="UILabel"/>
+                <relationship kind="outlet" name="table" candidateClass="UITableView"/>
+                <relationship kind="outlet" name="vibrateCellView" candidateClass="UIView"/>
+            </relationships>
+        </class>
+        <class className="RegisterViewController" superclassName="UIViewController">
+            <source key="sourceIdentifier" type="project" relativePath="./Classes/RegisterViewController.h"/>
+            <relationships>
+                <relationship kind="outlet" name="emailField" candidateClass="UITextField"/>
+                <relationship kind="outlet" name="entireScreen" candidateClass="UIView"/>
+                <relationship kind="outlet" name="license" candidateClass="UITextField"/>
+                <relationship kind="outlet" name="passwordField" candidateClass="UITextField"/>
+            </relationships>
+        </class>
+        <class className="SelfReportingViewController" superclassName="UIViewController">
+            <source key="sourceIdentifier" type="project" relativePath="./Classes/SelfReportingViewController.h"/>
+            <relationships>
+                <relationship kind="outlet" name="leftButton" candidateClass="UIBarButtonItem"/>
+                <relationship kind="outlet" name="mapView" candidateClass="MKMapView"/>
+                <relationship kind="outlet" name="rightButton" candidateClass="UIBarButtonItem"/>
+            </relationships>
+        </class>
+    </classes>
     <simulatedMetricsContainer key="defaultSimulatedMetrics">
         <simulatedStatusBarMetrics key="statusBar"/>
         <simulatedOrientationMetrics key="orientation"/>